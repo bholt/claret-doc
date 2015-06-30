1027ebc9648d603447f819f846cbc6e37d369946
Brandon Holt <bholt@cs.washington.edu>
2015-06-30 16:12:26 -0700

    histogram for bid-dist, improve auction-conflicts table

--- full/plots/bid-dist.r
+++ full/plots/bid-dist.r
@@ -11,13 +11,24 @@ df1 <- data.frame(x=num(names(l1))+0.02, y=vals(l1))
 df1 <- df1[order(df1$x),]
 df1v <- as.vector(rep(df1$x,  df1$y))
 
+# save(
+#   ggplot(data.frame(x=df1v), aes(x=x))+
+#     # geom_histogram(binwidth=0.02, fill=c.blue)+
+#     stat_ecdf(color=c.blue)+
+#     # geom_line()+
+#     # geom_density(fill=NA, color=c.blue, adjust=1/2)+
+#     scale_x_continuous(limits=c(0,1.0))+
+#     xlab('Percentage auction time')+ylab('# of bids')+
+#     theme_mine
+# , w=3.4, h=3.0)
+print("before save")
+
 save(
-  ggplot(data.frame(x=df1v, aes(x=x))+
-    # geom_histogram(binwidth=0.02, fill=c.blue)+
-    stat_ecdf(color=c.blue)+
-    # geom_line()+
-    # geom_density(fill=NA, color=c.blue, adjust=1/2)+
-    scale_x_continuous(limits=c(0,1.0))+
+  ggplot(df1, aes(x=x, weight=y))+
+    geom_histogram(binwidth=0.02, fill=c.blue)+
+    # scale_x_continuous(limits=c(0,1.0))+
+    xlim(0,1.0)+ylim(0,50000)+
     xlab('Percentage auction time')+ylab('# of bids')+
     theme_mine
-, w=3.4, h=3.0)
+, w=3.4, h=2.0)
+print("after save")
