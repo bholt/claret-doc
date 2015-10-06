fb7a3b91f1092c5e3bb33ea5dc68d3710ab1b1a0
Brandon Holt <bholt@cs.washington.edu>
2015-10-05 23:11:05 -0700

    rubis-conflicts: save/restore to csv

--- full/plots/rubis-conflicts.r
+++ full/plots/rubis-conflicts.r
@@ -1,62 +1,68 @@
 #!/usr/bin/env Rscript
 source('common.r')
 
-d <- data.rubis(where="duration = 60 and name like 'v0.28.1%' and nthreads <= 96 and server_txn_conflicts != 'NA'")
+by.conflict.mean <- data.or.csv(
+  csv = 'data/rubis-conflicts.csv',
+  gen = function(){
+    d <- data.rubis(where="duration = 60 and name like 'v0.28.1%' and nthreads <= 96 and server_txn_conflicts != 'NA'")
 
-# convert tag # to names
-parse_txn_conflicts <- function(r) {
-  tags <- c(
-    '0'='?',
-    '1'='AddUser',
-    '2'='ViewUser',
-    '3'='Open',
-    '4'='Close',
-    '5'='Bid',
-    '6'='Browse',
-    '7'='View',
-    '8'='Comment'
-  )
-  s <- r$server_txn_conflicts
-  for (k in names(tags)) {
-    s <- gsub('\\('+k, tags[[k]], s)
-    s <- gsub(','+k, ','+tags[[k]], s)
-  }
-  s <- gsub('\\)','',s)
-  fromJSON(jsfix(s))
-}
+    # convert tag # to names
+    parse_txn_conflicts <- function(r) {
+      tags <- c(
+        '0'='?',
+        '1'='AddUser',
+        '2'='ViewUser',
+        '3'='Open',
+        '4'='Close',
+        '5'='Bid',
+        '6'='Browse',
+        '7'='View',
+        '8'='Comment'
+      )
+      s <- r$server_txn_conflicts
+      for (k in names(tags)) {
+        s <- gsub('\\('+k, tags[[k]], s)
+        s <- gsub(','+k, ','+tags[[k]], s)
+      }
+      s <- gsub('\\)','',s)
+      fromJSON(jsfix(s))
+    }
 
-by.conflict <- adply(d, 1, function(r){
-    j <- parse_txn_conflicts(r)    
-    z <- function(n) { v <- j[[n]]; if (is.null(v)) 0 else v }
-    l <- list(
-        'Open-Open'         = z('Open'), z('Open,Open'),
-        'Open-Close'        = z('Open,Close') + z('Close,Open'),
-        'Bid-Bid'           = z('Bid') + z('Bid,Bid'),
-        # 'Bid-Close'         = z('Bid,Close') + z('Close,Bid'),
-        'Bid-View'          = z('Bid,View') + z('View,Bid'),
-        'Browse-Open/Close' = z('Browse,Close') + z('Close,Browse') +
-                              z('Browse,Open') + z('Open,Browse')
-    )    
-    f <- data.frame(
-        tag = names(l),
-        count = vals(l)
-    )
+    by.conflict <- adply(d, 1, function(r){
+        j <- parse_txn_conflicts(r)    
+        z <- function(n) { v <- j[[n]]; if (is.null(v)) 0 else v }
+        l <- list(
+            'Open-Open'         = z('Open'), z('Open,Open'),
+            'Open-Close'        = z('Open,Close') + z('Close,Open'),
+            'Bid-Bid'           = z('Bid') + z('Bid,Bid'),
+            # 'Bid-Close'         = z('Bid,Close') + z('Close,Bid'),
+            'Bid-View'          = z('Bid,View') + z('View,Bid'),
+            'Browse-Open/Close' = z('Browse,Close') + z('Close,Browse') +
+                                  z('Browse,Open') + z('Open,Browse')
+        )    
+        f <- data.frame(
+            tag = names(l),
+            count = vals(l)
+        )
     
-    f
-})
+        f
+    })
 
-by.conflict <- subset(by.conflict, tag != '')
+    by.conflict <- subset(by.conflict, tag != '')
 
-by.conflict$tag <- factor(by.conflict$tag, levels = c(
-  'Open-Open',
-  'Open-Close',
-  # 'Bid-Close',
-  'Browse-Open/Close',
-  'Bid-View',
-  'Bid-Bid'
-))
+    by.conflict$tag <- factor(by.conflict$tag, levels = c(
+      'Open-Open',
+      'Open-Close',
+      # 'Bid-Close',
+      'Browse-Open/Close',
+      'Bid-View',
+      'Bid-Bid'
+    ))
 
-by.conflict.mean <- ddply(by.conflict, .(cc_ph, tag), summarize, count = mean(count))
+    by.conflict.mean <- ddply(by.conflict, .(cc_ph, tag), summarize, count = mean(count))
+    
+    by.conflict.mean
+  })
 
 rw.count <- subset(by.conflict.mean, tag == 'Bid-Bid' & cc_ph == RW+BASE)$count
 
