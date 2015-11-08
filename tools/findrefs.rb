26fe4e788e6bf287f366d0fd6225b88513f652c8
Brandon Holt <bholt@cs.washington.edu>
2015-11-08 12:10:06 -0800

    findrefs: skip the search if we don't have allbib

--- tools/findrefs.rb
+++ tools/findrefs.rb
@@ -13,37 +13,40 @@ changed = false
 
 # bib file to search within for missing references
 allbib = "#{ENV['HOME']}/Dropbox/all.bib"
-search_bib = BibTeX.open(allbib) if File.exist? allbib
+if File.exist? allbib
+  search_bib = BibTeX.open(allbib) if File.exist? allbib
 
-citekeys.each do |k|
-  p = proj_bib[k]
-  s = search_bib[k]
+  citekeys.each do |k|
+    p = proj_bib[k]
+    s = search_bib[k]
     
-  if p and s
-    # s has a newer modified date
-    if p["date-modified"] and s["date-modified"] and s["date-modified"] > p["date-modified"]
-      puts "updating #{k}"
-      proj_bib.remove(k)
+    if p and s
+      # s has a newer modified date
+      if p["date-modified"] and s["date-modified"] and s["date-modified"] > p["date-modified"]
+        puts "updating #{k}"
+        proj_bib.remove(k)
+        proj_bib.add(s)
+        changed = true
+      end
+    elsif !p and s
+      puts "adding #{k}"
       proj_bib.add(s)
       changed = true
-    end
-  elsif !p and s
-    puts "adding #{k}"
-    proj_bib.add(s)
-    changed = true
-  elsif !p and !s
-    $stderr.write("warning: unable to find citekey: #{k}\n")
-  end
-end
-
-if changed
-  # remove BibDesk fields
-  proj_bib.each do |e|
-    fields = e.to_hash.keys.keep_if{|k| k =~ /^bdsk/}
-    if not fields.empty?
-      fields.each{|f| e.delete(f) }
+    elsif !p and !s
+      $stderr.write("warning: unable to find citekey: #{k}\n")
     end
   end
-  # save back to file 'refs.bib' file
-  proj_bib.save_to(f)
+  
+  if changed
+    # remove BibDesk fields
+    proj_bib.each do |e|
+      fields = e.to_hash.keys.keep_if{|k| k =~ /^bdsk/}
+      if not fields.empty?
+        fields.each{|f| e.delete(f) }
+      end
+    end
+    # save back to file 'refs.bib' file
+    proj_bib.save_to(f)
+  end
+  
 end
