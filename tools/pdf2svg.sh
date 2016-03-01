d6c890c3d0c78adcaedff81f8f40c0a1a66f9080
Brandon Holt <bholt@cs.washington.edu>
2016-03-01 12:44:55 -0800

    ipa paper skeleton, rename plot -> plots

--- tools/pdf2svg.sh
+++ tools/pdf2svg.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ -d plots ]; then
-  for f in plots/*.pdf; do
-    pdf2svg $f ${f/.pdf/.svg}
-  done
+  if test -n "$(shopt -s nullglob; echo plots/*.pdf)"; then
+    for f in plots/*.pdf; do
+      pdf2svg $f ${f/.pdf/.svg}
+    done
+  fi
 fi
