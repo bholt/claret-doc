fdeb3de2021536dcf0675f7a421c325b08f59929
Brandon Holt <bholt@cs.washington.edu>
2016-03-01 23:58:56 -0800

    script to launch and configure container and run make jobs

--- hooknook.sh
+++ hooknook.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 container=bholt-claret-doc
-docker run --name=$container -d bholt/claret-doc tail -f /dev/null
-
 dexec() { docker exec -i $container sh -c "$@"; }
 
-dexec 'mkdir -p ~/.ssh'
-dexec 'cat > ~/.ssh/id_rsa; chmod go-r ~/.ssh/id_rsa' < ../../keys/id_rsa
-dexec 'cat > ~/.ssh/id_rsa.pub' < ../../keys/id_rsa.pub
-dexec "echo 'Host *\nStrictHostKeyChecking no' > ~/.ssh/config"
-dexec 'git clone git@github.com:bholt/claret-doc.git'
-dexec 'git config --global user.name "Hooknook"'
-dexec 'git config --global user.email "<>"'
-dexec 'git config --global push.default matching'
-dexec 'cd claret-doc; git checkout gh-pages; git checkout master; git worktree add .pages gh-pages'
+# if container doesn't already exist, then create it
+if [ $(docker ps | grep $container | wc -l) -eq '0' ]; then
 
-dexec 'cd claret-doc; make deploy'
+  docker run --name=$container -d bholt/claret-doc tail -f /dev/null
+
+  dexec 'mkdir -p ~/.ssh'
+  dexec 'cat > ~/.ssh/id_rsa; chmod go-r ~/.ssh/id_rsa' < ../../keys/id_rsa
+  dexec 'cat > ~/.ssh/id_rsa.pub' < ../../keys/id_rsa.pub
+  dexec "echo 'Host *\nStrictHostKeyChecking no' > ~/.ssh/config"
+  dexec 'git clone git@github.com:bholt/claret-doc.git'
+  dexec 'git config --global user.name "Hooknook"'
+  dexec 'git config --global user.email "<>"'
+  dexec 'git config --global push.default matching'
+  dexec 'cd claret-doc; git checkout gh-pages; git checkout master; git worktree add .pages gh-pages'
+
+  dexec 'cd claret-doc; make deploy'
+
+else # if container exists, then just pull latest and re-run
+  
+  dexec 'cd claret-doc && git pull && make deploy'
+  
+fi
