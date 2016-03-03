fd85a1f37f238928825c7f10a18e57795ac52a6c
Brandon Holt <bholt@cs.washington.edu>
2016-03-03 14:26:47 -0800

    add script for remote-build (rsync-docker-rsync)

---
+++ remote.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DIR="${BASH_SOURCE%/*}"
+cd $DIR
+server=ibex.syslab
+dest='/scratch/$USER/claret-doc'
+echo ">>> copying files to $server:$dest/.."
+ssh $server mkdir -p $dest
+rsync -a . $server:$dest
+echo ">>> building remotely"
+ssh $server "cd $dest; docker run --rm -i -v $dest:/src bholt/claret-doc $@"
+echo ">>> copying files back"
+rsync -a $server:$dest ..
